563476c12e59946992645790e78681926fab583a
Gerald J Astcio <jerard_17@hotmail.com>
2022-09-24 16:21:44 -0400

    actualizacion 0.04

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -14,9 +14,10 @@ const Weather = ({ water, imagen }) => {
     }
 
 
-   let iconUrl = water?.weather[0].icon
+    let url = "http://openweathermap.org/img/wn/"
+    let iconUrl = url + water?.weather[0].icon + "@4x.png"
 
-
+   // <p className="card-icon"><img src={water ? `http://openweathermap.org/img/wn/${iconUrl}@4x.png`: ""} alt="icon-temp" /></p>
 
 
     return (
@@ -25,12 +26,12 @@ const Weather = ({ water, imagen }) => {
             <img className="card-img" src={imagen} alt="imagen-RD" />
             <h1 className="card-title">Weather App</h1>
             <h2 className='card-city'>{`${water?.name}, ${water?.sys.country}`}</h2>
-            <p className="card-icon"><img src={water ? `http://openweathermap.org/img/wn/${iconUrl}@4x.png`: ""} alt="icon-temp" /></p>
+            <p className="card-icon"><img src={ water ? iconUrl : ""} alt="icon-temp" /></p>
 
             <p className="card-clouds">"{water?.weather[0].description}"</p>
-            <p className="speed"><i class="fa-solid fa-wind"> </i><span>Wind Speed:</span> {water?.wind.speed}m/s</p>
-            <p className="Clouds"><i class="fa-solid fa-cloud"></i><span>Clouds:</span> {water?.clouds.all}%</p>
-            <p className="Pressure"><i class="fa-solid fa-temperature-three-quarters"></i><span className="pressuree">Pressure:</span> {water?.main.pressure}hPa</p>
+            <p className="speed"><i className="fa-solid fa-wind"> </i><span>Wind Speed:</span> {water?.wind.speed}m/s</p>
+            <p className="Clouds"><i className="fa-solid fa-cloud"></i><span>Clouds:</span> {water?.clouds.all}%</p>
+            <p className="Pressure"><i className="fa-solid fa-temperature-three-quarters"></i><span className="pressuree">Pressure:</span> {water?.main.pressure}hPa</p>
 
 
             <button onClick={handleButton} className="card-btn">Degrees °C/°F </button>
